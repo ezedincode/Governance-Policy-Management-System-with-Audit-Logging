52468160eef7924a2aad534bd2492cef3394bbb9
Ezedin Ahmed <ezedinadefa@gmail.com>
2026-05-31 11:50:31 +0300

    initializing Governance_Service using spring initializer

---
+++ Governance_Service/src/main/java/com/ezedin/Governance_Service/GovernanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ezedin.Governance_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernanceServiceApplication.class, args);
+	}
+
+}
---
+++ Governance_Service/src/test/java/com/ezedin/Governance_Service/GovernanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezedin.Governance_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
